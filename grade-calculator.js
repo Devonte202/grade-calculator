e76d1023ca103d2c85d7cc93b59186184174e653
Devy <devonteduncan202@gmail.com>
2018-01-28 04:01:51 -0500

    Front End Complete

--- grade-calculator.js
+++ grade-calculator.js
@@ -1,20 +1,35 @@
-function calcGrade(h,x,y,z){
+$(document).ready(function(){
 
-  var Grade = (0.10*h)+(0.25*x)+(0.25*y)+(0.40*z)
-  return Grade ;
-}
-console.log("Calculus " + calcGrade(84.72,77,55,82).toFixed(4));
-//New Class
-function physGrade(h,x,y,z){
+$("#addAspect").click(function(){
 
-  var Grade = (0.10*h)+(0.20*x)+(0.20*y)+(0.50*z)
-  return Grade ;
-};
-console.log("Physics " + physGrade(100,90,45,100).toFixed(4));
-//New Class
-function civGrade(m,p,f,t){
+   $("#inputTable").append("\
+   <tr id="tableItem">\
+     <td>\
+       <form>\
+             <input list="aspects" name="aspects">\
+             <datalist id="aspects">\
+               <option value="Exams">\
+               <option value="Homework">\
+               <option value="Attendance">\
+               <option value="Term paper(s)">\
+               <option value="Quizzes">\
+               <option value="Final Exam">\
+               <option value="Lab">\
+           </datalist>\
+       </form>\
+     </td>\
+     <td>\
+       <form>\
+             <input class="percentInput">\
+       </form>\
+     </td>\
+     <td>\
+       <form>\
+             <input class="gradeInput">\
+       </form>\
+     </td>\
+   </tr>\
+      ")
 
-  var Grade = (0.30*m)+(0.10*p)+(0.40*f)+(0.20*t)
-  return Grade ;
-};
-console.log("World Civ " + civGrade(80,80,70,84).toFixed(4));
+    });
+       });
