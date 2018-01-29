b42f94273947a5b0834a4d4a02dd2b35a7aa327c
Devy <devonteduncan202@gmail.com>
2018-01-29 03:02:18 -0500

    Missing Button & Logic

--- grade-calculator.js
+++ grade-calculator.js
@@ -1,35 +1,59 @@
 $(document).ready(function(){
-
+// Start of add aspect button code
 $("#addAspect").click(function(){
-
-   $("#inputTable").append("\
-   <tr id="tableItem">\
+    $("#inputTable").append("\
+   <tr class='tableItem'>\
      <td>\
        <form>\
-             <input list="aspects" name="aspects">\
-             <datalist id="aspects">\
-               <option value="Exams">\
-               <option value="Homework">\
-               <option value="Attendance">\
-               <option value="Term paper(s)">\
-               <option value="Quizzes">\
-               <option value="Final Exam">\
-               <option value="Lab">\
+             <input list='aspects' name='aspects'>\
+             <datalist id='aspects'>\
+               <option value='Exams'>\
+               <option value='Homework'>\
+               <option value='Attendance'>\
+               <option value='Term paper(s)'>\
+               <option value='Quizzes'>\
+               <option value='Final Exam'>\
+               <option value='Lab'>\
            </datalist>\
        </form>\
      </td>\
      <td>\
        <form>\
-             <input class="percentInput">\
+             <input class='percentInput'>\
        </form>\
      </td>\
      <td>\
        <form>\
-             <input class="gradeInput">\
+             <input class='gradeInput'>\
        </form>\
      </td>\
    </tr>\
       ")
 
-    });
-       });
+   });
+// End of add aspect button code
+
+
+
+// Start of Calculate Button Code
+
+      // function calcGrade(h,x,y,z){
+     //var Grade = (0.10*h)+(0.25*x)+(0.25*y)+(0.40*z)
+    //  return Grade ;
+    //}
+   //console.log("Calculus " + calcGrade(84.72,77,55,82).toFixed(4));
+   // FIGURE OUT HOW TO MAKE THIS LOGIC DYNAMIC 
+
+// Code written here is to check to see if the data in forms was being collected
+   var percentages = $(".percentInput");
+   $("#calculate").click(function(){
+     var sum = 0;
+
+$('.percentInput').each(function(){
+    sum += parseInt(this.value);
+});
+      console.log(sum);
+
+     });
+
+});
